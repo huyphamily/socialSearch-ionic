a87ca9296b6703689174e8b1234f199c2a9454eb
Justin Cruz <digikid13@gmail.com>
2014-09-09 14:56:04 -0400

    removed invalid folder

--- platform/android/ant-build/socialSearch-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-c:\Users\Nameda\Desktop\Hack Reactor Stuff\test stuff\socialsearch-ionic\platforms\android\ant-build\socialSearch-debug-unaligned.apk : \
-c:\Users\Nameda\Desktop\Hack Reactor Stuff\test stuff\socialsearch-ionic\platforms\android\ant-build\socialSearch.ap_ \
-c:\Users\Nameda\Desktop\Hack Reactor Stuff\test stuff\socialsearch-ionic\platforms\android\ant-build\classes.dex \
-C:\Users\Nameda\Desktop\Hack Reactor Stuff\test stuff\socialsearch-ionic\platforms\android\CordovaLib\ant-build\classes.jar \
